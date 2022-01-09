05c6323eb39a9d5f329b241d310565da1656fd5f
Jesse Douglas <jesse@jesdosoftware.com>
2022-01-09 09:47:42 -0500

    Add and/xor functions

--- src/main.c
+++ src/main.c
@@ -14,17 +14,52 @@ long random_range(int lower, int upper)
     return random() % (upper - lower + 1) + lower;
 }
 
-void populate_initial_byte_field()
+bool bool_and(bool x, bool y)
 {
-    int start_pos = random_range(0, 7);
-    byte_fields[0][start_pos] = 1;
+    return x && y;
 }
 
-void populate_random_byte_field(int row)
+bool bool_xor(bool x, bool y)
+{
+    return (x && !y) || (!x && y);
+}
+
+void populate_initial_byte_field(bool *byte_field)
+{
+    int start_pos = random_range(0, 7);
+    byte_field[start_pos] = 1;
+}
+
+void populate_random_byte_field(bool *byte_field)
 {
     for (int i = 0; i < BYTE_FIELD_SIZE; ++i)
     {
-        byte_fields[row][i] = random() % 2;
+        byte_field[i] = random() % 2;
+    }
+}
+
+void print_byte_field(bool const *byte_field)
+{
+    for (int i = 0; i < BYTE_FIELD_SIZE; ++i)
+    {
+        printf("%i", byte_field[i]);
+    }
+    printf("\n");
+}
+
+void and_byte_fields(bool const *first, bool const *second, bool *out)
+{
+    for (int i = 0; i < BYTE_FIELD_SIZE; ++i)
+    {
+        out[i] = bool_and(first[i], second[i]);
+    }
+}
+
+void xor_byte_fields(bool const *first, bool const *second, bool *out)
+{
+    for (int i = 0; i < BYTE_FIELD_SIZE; ++i)
+    {
+        out[i] = bool_xor(first[i], second[i]);
     }
 }
 
@@ -37,15 +72,23 @@ void create_byte_fields(int rows)
         byte_fields[row] = calloc(BYTE_FIELD_SIZE, sizeof(bool));
         if (row == 0)
         {
-            populate_initial_byte_field();
+            populate_initial_byte_field(byte_fields[row]);
         }
-        else
+        else if (row == 1)
         {
-            populate_random_byte_field(row);
+            populate_random_byte_field(byte_fields[row]);
         }
     }
 
     byte_field_rows = rows;
+}
+
+void print_byte_fields()
+{
+    for (int i = 0; i < byte_field_rows; ++i)
+    {
+        print_byte_field(byte_fields[i]);
+    }
 }
 
 void free_byte_fields()
@@ -55,23 +98,6 @@ void free_byte_fields()
         free(byte_fields[i]);
     }
     free(byte_fields);
-}
-
-void print_byte_field(int row)
-{
-    for (int i = 0; i < BYTE_FIELD_SIZE; ++i)
-    {
-        printf("%i", byte_fields[row][i]);
-    }
-    printf("\n");
-}
-
-void print_byte_fields()
-{
-    for (int i = 0; i < byte_field_rows; ++i)
-    {
-        print_byte_field(i);
-    }
 }
 
 int main()
