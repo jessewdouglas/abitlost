504455bf9e570a0c18634ec96b19f9f3173d69ee
Jesse Douglas <jesse@jesdosoftware.com>
2022-01-10 09:01:40 -0500

    Add byte population from random operations

--- src/main.c
+++ src/main.c
@@ -3,108 +3,95 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define BYTE_FIELD_SIZE 8
+#define BYTE_SIZE 8
 
-bool **byte_fields;
-int byte_field_rows;
+bool **bytes;
+int byte_rows;
 int current_row;
 
-long random_range(int lower, int upper)
-{
+long random_range(int lower, int upper) {
     return random() % (upper - lower + 1) + lower;
 }
 
-bool bool_and(bool x, bool y)
-{
-    return x && y;
-}
+bool and_bools(bool x, bool y) { return x && y; }
 
-bool bool_xor(bool x, bool y)
-{
-    return (x && !y) || (!x && y);
-}
+bool xor_bools(bool x, bool y) { return (x && !y) || (!x && y); }
 
-void populate_initial_byte_field(bool *byte_field)
-{
-    int start_pos = random_range(0, 7);
-    byte_field[start_pos] = 1;
-}
-
-void populate_random_byte_field(bool *byte_field)
-{
-    for (int i = 0; i < BYTE_FIELD_SIZE; ++i)
-    {
-        byte_field[i] = random() % 2;
+void and_bytes(bool const *first, bool const *second, bool *out) {
+    for (int i = 0; i < BYTE_SIZE; ++i) {
+        out[i] = and_bools(first[i], second[i]);
     }
 }
 
-void print_byte_field(bool const *byte_field)
-{
-    for (int i = 0; i < BYTE_FIELD_SIZE; ++i)
-    {
-        printf("%i", byte_field[i]);
+void xor_bytes(bool const *first, bool const *second, bool *out) {
+    for (int i = 0; i < BYTE_SIZE; ++i) {
+        out[i] = xor_bools(first[i], second[i]);
+    }
+}
+
+void populate_initial_byte(bool *out) {
+    int start_pos = random_range(0, 7);
+    out[start_pos] = 1;
+}
+
+void populate_random_byte(bool *out) {
+    for (int i = 0; i < BYTE_SIZE; ++i) {
+        out[i] = random() % 2;
+    }
+}
+
+void populate_byte_from_operation(bool const *operand, bool *out) {
+    bool temp[BYTE_SIZE];
+    populate_random_byte(temp);
+
+    if (random() % 2) {
+        and_bytes(operand, temp, out);
+    } else {
+        xor_bytes(operand, temp, out);
+    }
+}
+
+void print_byte(bool const *byte) {
+    for (int i = 0; i < BYTE_SIZE; ++i) {
+        printf("%i", byte[i]);
     }
     printf("\n");
 }
 
-void and_byte_fields(bool const *first, bool const *second, bool *out)
-{
-    for (int i = 0; i < BYTE_FIELD_SIZE; ++i)
-    {
-        out[i] = bool_and(first[i], second[i]);
-    }
-}
+void create_byte_rows(int rows) {
+    bytes = calloc(rows, sizeof(bool *));
 
-void xor_byte_fields(bool const *first, bool const *second, bool *out)
-{
-    for (int i = 0; i < BYTE_FIELD_SIZE; ++i)
-    {
-        out[i] = bool_xor(first[i], second[i]);
-    }
-}
-
-void create_byte_fields(int rows)
-{
-    byte_fields = calloc(rows, sizeof(bool *));
-
-    for (int row = 0; row < rows; ++row)
-    {
-        byte_fields[row] = calloc(BYTE_FIELD_SIZE, sizeof(bool));
-        if (row == 0)
-        {
-            populate_initial_byte_field(byte_fields[row]);
-        }
-        else if (row == 1)
-        {
-            populate_random_byte_field(byte_fields[row]);
+    for (int row = 0; row < rows; ++row) {
+        bytes[row] = calloc(BYTE_SIZE, sizeof(bool));
+        if (row == 0) {
+            populate_initial_byte(bytes[row]);
+        } else if (row == 1) {
+            populate_random_byte(bytes[row]);
+        } else {
+            populate_byte_from_operation(bytes[row - 1], bytes[row]);
         }
     }
 
-    byte_field_rows = rows;
+    byte_rows = rows;
 }
 
-void print_byte_fields()
-{
-    for (int i = 0; i < byte_field_rows; ++i)
-    {
-        print_byte_field(byte_fields[i]);
+void print_bytes() {
+    for (int i = 0; i < byte_rows; ++i) {
+        print_byte(bytes[i]);
     }
 }
 
-void free_byte_fields()
-{
-    for (int i = 0; i < byte_field_rows; ++i)
-    {
-        free(byte_fields[i]);
+void free_bytes() {
+    for (int i = 0; i < byte_rows; ++i) {
+        free(bytes[i]);
     }
-    free(byte_fields);
+    free(bytes);
 }
 
-int main()
-{
+int main() {
     srandom(time(0));
 
-    create_byte_fields(4);
-    print_byte_fields();
-    free_byte_fields();
+    create_byte_rows(4);
+    print_bytes();
+    free_bytes();
 }
